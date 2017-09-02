a3ff56e013930553bd042c9c02f89252732243d6
Even Hu <328345551@qq.com>
2017-09-03 00:08:26 +0800

    增加服务端expres跨域

--- server/app.js
+++ server/app.js
@@ -25,6 +25,13 @@ app.use(bodyParser.urlencoded({
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.all('/', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
 app.use('/', index)
 app.use('/users', users)
 app.use('/goods', goods)
