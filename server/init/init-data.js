913f727c82c0b4f89487e2db6bc63fec59c56f1b
zhi3210happy <328345551@qq.com>
2017-12-01 15:34:01 +0800

    请求地址默认改成线上

--- server/init/init-data.js
+++ server/init/init-data.js
@@ -1,6 +1,6 @@
 // 初始化数据库
-var getGoods = require('./getGoods')
-let getAdmin = require('./admin')
+const getGoods = require('./getGoods')
+const getAdmin = require('./admin')
 
 getGoods()
 getAdmin()
